5b312d28927d2d2aa20b5aed382b0811a7ca456e
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2022-09-03 19:10:24 +0300

    update file

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int clientAccount = 100;//начальная сумма на счете клиента
+        int bonusRubles = 100;//бонусные рубли за каждые 100 рублей
+        int deposit = 1100;// сумма пополнения счета
+        int bonus;//сумма бонусов
+
+        if (deposit >= 1000){
+            bonus = deposit / bonusRubles;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = clientAccount + deposit + bonus;
+
+        System.out.println("Спасибо , Ваш платеж зачислен. Ваш баланс : " + deposit + " руб.");
+        System.out.println("Ваш бонус составил " + bonus + " руб.");
+        System.out.println("Итоговая сумма на счету : " + balance + " руб.");
+    }
+    }
+
+
